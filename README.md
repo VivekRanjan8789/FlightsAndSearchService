094a5ba5ff2bf447b05f0cb112939c4c60e1133a
vivek <vivekranjan8789@gmail.com>
2024-03-31 12:15:40 +0530

    Added unique constraint and started cityrepository

--- README.md
+++ README.md
@@ -20,5 +20,18 @@
 
 ```
 - once you have added your db config as listed above, go to the src folder from your terminal and execute `npx sequelize db:create`
+and then execute
+
+`npx sequelize db:migrate`
 ```
-```
+
+
+## DB Design
+   - Airplane Table
+   - Flight
+   - Airport
+   - City
+
+   - A flight belongs to an irplane can be used in multiple flights
+   - A city has many airports but one airport belongs to a city
+   - One airport can have many flights, but a flight belongs to one airport
--- src/index.js
+++ src/index.js
@@ -1,19 +1,26 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
-const { PORT } = require('./config/serverConfig.js')
+
+const  cityRepository  = require('./repository/city-repository');
+const { PORT } = require('./config/serverConfig.js');
+
 
 const setupAndStartServer = async () => {
 
       //create the express object
       const app = express();
-
+-
+      app.use(bodyParser.json());
       app.use(bodyParser.urlencoded({ extended: true}));
 
-      app.listen(PORT, ()=>{
+      app.listen(PORT, async ()=>{
         console.log(`server started at PORT ${PORT}`);
         
+        const repo = new cityRepository();
+        repo.createCity({name: "Bihar Sharif"});
       });
 }
+
 
 setupAndStartServer();  
