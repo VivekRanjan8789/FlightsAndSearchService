93016beada4277784b142e088a2cbfa23f33dc71
vivek <vivekranjan8789@gmail.com>
2024-04-04 02:10:35 +0530

    Added Airport Seeder

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ const bodyParser = require('body-parser');
 const { PORT } = require('./config/serverConfig.js');
 const ApiRoutes = require('./routes/index.js');
 
+const db = require('./models/index.js');
 
 const setupAndStartServer = async () => {
 
@@ -18,7 +19,14 @@ const setupAndStartServer = async () => {
       app.use('/api', ApiRoutes);  
 
       app.listen(PORT, async ()=>{
+
         console.log(`server started at PORT ${PORT}`);
+
+        if(process.env.SYNC_DB){
+            db.sequelize.sync({alter: true});
+        }
+      
+            
       });
 }
 
