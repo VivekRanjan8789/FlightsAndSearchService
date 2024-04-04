7eb0362cf773566a8d009a5e9c7dfa1affb84b39
vivek <vivekranjan8789@gmail.com>
2024-04-04 19:24:35 +0530

    Added airplanes

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
-
+const {Airplane} = require('./models/index.js');
 
 const { PORT } = require('./config/serverConfig.js');
 const ApiRoutes = require('./routes/index.js');
@@ -25,7 +25,10 @@ const setupAndStartServer = async () => {
         if(process.env.SYNC_DB){
             db.sequelize.sync({alter: true});
         }
-      
+        
+         await Airplane.create({
+            modelNumber: 'Bombardier CRJ'
+         });
             
       });
 }
