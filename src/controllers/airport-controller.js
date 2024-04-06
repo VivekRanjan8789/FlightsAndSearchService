b97a124ff5a53cb7ddcd0180fa083834c9924de8
vivek <vivekranjan8789@gmail.com>
2024-04-06 16:04:11 +0530

    Added flight create request middleware

--- src/controllers/airport-controller.js
+++ src/controllers/airport-controller.js
@@ -4,13 +4,24 @@ const airportService = new AirportService();
 
 const create = async(req,res) => {
        try {
-          const response = await airportService.create(req.body);
+        const flightRequestData = {
+            flightNumber: req.body.flightNumber,
+            airplaneId: req.body.airplaneId,
+            departureAirportId: req.body.departureAirportId,
+            arrivalAirportId: req.body.arrivalAirportId,
+            arrivalTime: req.body.arrivalTime,
+            departureTime: req.body.departureTime,
+            price: req.body.price
+        }
+          const response = await airportService.create(flightRequestData);
+          console.log(req.body);
           return res.status(201).json({
               data: response,
               success: true,
               message: "successfully creasted an airport",
               err: {}
           })
+          
        } catch (error) {
         console.log(error);
         return res.status(500).json({
---
+++ src/middleware/index.js
@@ -0,0 +1,8 @@
+module.exports = {
+    FlightMiddlwares: require('./flight-middlewares')
+}
+// console.log(FlightMiddlwares);
+
+// module.exports = {
+//     FlightMiddlewares: require('../middleware/flight-middlewares')
+// }
